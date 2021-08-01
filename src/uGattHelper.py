216c5cab9b2e3bca2e2b44cd42ac58842f45ac9d
Jiho <jihotadano@mailbox.org>
2021-08-02 01:14:09 +0200

    Rename helper file to make it better distinguishable

--- src/uGattHelper.py
+++ src/uGattHelper.py
@@ -1,12 +1,39 @@
 import JSONHelper
+import json
 from uGatt import getBackend, format_input
 from datetime import datetime
 
-def getUUID(json, softwareRevision, characteristic):
-    return JSONHelper.getCharacteristicUUID(json, softwareRevision, characteristic)
 
-def getHandle(json, softwareRevision, characteristic):
-    return JSONHelper.getCharacteristicHandle(json, softwareRevision, characteristic)
+def getDevices(root):
+    devices = []
+
+    for node in JSONHelper.getNodes(root)["Devices"]:
+        devices.append(node["Firmware"])
+
+    return devices
+
+
+def getUUID(root, firmware, characteristic):
+    for node in JSONHelper.getNodes(root)["Devices"]:
+        if node["Firmware"] == firmware:
+            for uuid in node["UUID"]:
+                uuidInfo = JSONHelper.getNodes(json.dumps(uuid))
+                if uuidInfo["Name"] == characteristic:
+                    return uuidInfo["UUID"]
+
+    return getUUID(root, "Default", characteristic)
+
+
+def getHandle(root, firmware, characteristic):
+    for node in JSONHelper.getNodes(root)["Devices"]:
+        if node["Firmware"] == firmware:
+            for uuid in node["UUID"]:
+                uuidInfo = JSONHelper.getNodes(json.dumps(uuid))
+                if uuidInfo["Name"] == characteristic:
+                    return uuidInfo["Handle"]
+
+    return getHandle(root, "Default", characteristic)
+
 
 def currentTimeToHex():
     currentTime = datetime.now()
@@ -20,6 +47,7 @@ def currentTimeToHex():
 
     return format_input([year[2:4], year[0:2], month, day, hour, minute, second])
 
+
 def parseToString(value):
     value = parseOutput(value)
     try:
@@ -27,10 +55,12 @@ def parseToString(value):
     except:
         return value.rstrip()
 
+
 def parseToInt(value):
     value = parseOutput(value)
 
     return int(str(value), 16)
+
 
 def parseOutput(output):
     value = output[0]
