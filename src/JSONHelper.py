0d2c48342199f141fb74ec3d5fb3a7e6ce91cc31
Jiho <jihotadano@mailbox.org>
2021-08-10 16:56:07 +0200

    Slim down, move specific functions to their own python files

--- src/JSONHelper.py
+++ src/JSONHelper.py
@@ -3,33 +3,18 @@ import os
 import posixpath
 from pathlib import Path
 
-def getDevices(root):
-    devices = []
-
-    for node in getNodes(root)["Devices"]:
-        devices.append(node["Firmware"])
-
-    return devices
-
-def getCharacteristicUUID(root, firmware, characteristic):
-    for node in getNodes(root)["Devices"]:
-        if node["Firmware"] == firmware:
-            for uuid in node["UUID"]:
-                uuidInfo = getNodes(json.dumps(uuid))
-                if uuidInfo["Name"] == characteristic:
-                    return uuidInfo["UUID"]
-
-    return getCharacteristicUUID(root, "Default", characteristic)
-
-def getCharacteristicHandle(root, firmware, characteristic):
-    for node in getNodes(root)["Devices"]:
-        if node["Firmware"] == firmware:
-            for uuid in node["UUID"]:
-                uuidInfo = getNodes(json.dumps(uuid))
-                if uuidInfo["Name"] == characteristic:
-                    return uuidInfo["Handle"]
-
-    return getCharacteristicHandle(root, "Default", characteristic)
 
 def getNodes(node):
     return json.loads(node)
+
+
+def getValue(node, key):
+    vals = []
+    for val in getNodes(node):
+        vals.append(val[key])
+
+    return vals
+
+
+def getAllValues(node):
+    return getNodes(node)
